686fc6e3ce4220b79d70c59fa7ddebff140749fe
ATarasov11 <kapitan-tarasov@mail.ru>
2024-06-05 23:09:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+a='Hello, world!'
+print(a)
+print(len(a))
+
+first=50
+second=30
+print(first+second)
+print(first-second)
+summa=80
+diff=20
+print(summa)
+print(diff)
+
+first=10
+second=20
+third=30
+print((first+second+third)//3)
+mean=20
+print(mean)
+
+first_string="Вторник"
+second_string="Понедельник"
+print(second_string+', '+first_string)
+
+a=2
+b=5
+c=7
+print((a*b)+(a*c))
+f=24
+print((f**3)/2)
